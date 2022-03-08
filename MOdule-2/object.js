24e73a27dbd9fbc78fae07dae237530947c9a1e6
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-08 22:17:09 +0530

    object in js

---
+++ MOdule-2/object.js
@@ -0,0 +1,27 @@
+// //empty object
+// let obj = {};
+// console.log(obj);
+
+// //object == key-pair value --> key name : value ,
+// let objAman = {
+//     "Name": " vishal bhanderi ",
+//     Age: 26,
+//     phoneNo: 9909802023,
+//     lastName: 'Gandhri'
+// }
+// console.log(objAman)
+
+let capAmerica = {
+    Name: 'steve',
+    Age: 9898,
+    Friends: ['Natasha', 'Thor', 'Tony', 'Hulk'],
+    address: {
+        city: 'surat',
+        state: 'Rajkot'
+    },
+    sayHi: function(){
+        console.log("Cap America says Namaste")
+    }
+}
+
+console.log(capAmerica);
