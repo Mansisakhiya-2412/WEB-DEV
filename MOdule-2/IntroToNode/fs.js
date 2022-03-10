ea287156e371cea9ad182184f45203e5f178d2a5
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-10 23:31:30 +0530

    crud for file complete

--- MOdule-2/IntroToNode/fs.js
+++ MOdule-2/IntroToNode/fs.js
@@ -6,5 +6,19 @@ let path = require("path");
 let FilePath = path.join(__dirname, "file.txt");
 console.log(FilePath);
 
+//C-create
 //it creates File i it does exists else it override
-fs.writeFileSync(FilePath, "HEllo i am vadapav  ")
+fs.writeFileSync(FilePath, "HEllo i am vadapav  ");
+
+//R-read
+let content = fs.readFileSync(FilePath, "utf-8");
+console.log(content);
+
+//U-update
+fs.appendFileSync(FilePath, "newly added content in File");
+console.log("Ater update : ");
+console.log(fs.readFileSync(FilePath,"utf-8"));
+
+//D-delete
+fs.unlinkSync(FilePath);
+
