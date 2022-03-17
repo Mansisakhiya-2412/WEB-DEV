2986f1237ed7cea87e925fd1bbc3bc8f1efcdcfd
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-17 21:34:22 +0530

    input

---
+++ MOdule-2/project/fileOrganizer.js
@@ -0,0 +1,8 @@
+// console.log("Hello World");
+
+let inputArr = process.argv;
+console.log(inputArr);
+//['pathTonode','pathtoFile','input_passed_in_terminal']
+
+let input = inputArr[2];
+console.log(input);
