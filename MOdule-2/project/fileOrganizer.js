430d8d7d92999610e2222ad6af8a65708cfaf7b0
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-19 00:24:39 +0530

    read dir

--- MOdule-2/project/fileOrganizer.js
+++ MOdule-2/project/fileOrganizer.js
@@ -1,8 +1,26 @@
-// console.log("Hello World");
+// // console.log("Hello World");
 
-let inputArr = process.argv;
-console.log(inputArr);
-//['pathTonode','pathtoFile','input_passed_in_terminal']
+// let inputArr = process.argv;
+// console.log(inputArr);
+// //['pathTonode','pathtoFile','input_passed_in_terminal']
 
-let input = inputArr[2];
-console.log(input);
+// let input = inputArr[2];
+// console.log(input);
+
+
+let fs = require("fs");
+let Folderpath = process.argv[2];
+
+let input = Folderpath[2];
+// console.log(Folderpath);
+
+let Folderexists = fs.existsSync(Folderpath);
+
+if (Folderexists) {
+    //We Will code
+    //console.log("Path is valid");
+    let Files = fs.readdirSync(Folderpath);
+    console.log(Files);
+} else {
+    console.log("Please Enter a valid Path!!!!!!!");
+}
