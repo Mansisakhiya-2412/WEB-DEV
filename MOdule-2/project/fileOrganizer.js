a57888517cbdc2dc7c2c8f923f142da2668a1418
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-19 22:46:49 +0530

    giveFolderName implemented

--- MOdule-2/project/fileOrganizer.js
+++ MOdule-2/project/fileOrganizer.js
@@ -9,18 +9,50 @@
 
 
 let fs = require("fs");
+let path = require("path");
+
 let Folderpath = process.argv[2];
 
-let input = Folderpath[2];
+//let input = Folderpath[2];
 // console.log(Folderpath);
 
 let Folderexists = fs.existsSync(Folderpath);
+
+let extensions = {
+    Audio: [".mp3"],
+    Video: [".mp4", ".mkv", ".gif"],
+    Document: [".doc", ".xlsx", ".pdf", ".txt"],
+    Image: [".jpeg", ".jpg", ".png"],
+    Software: [".exe", ".apk"]
+}
 
 if (Folderexists) {
     //We Will code
     //console.log("Path is valid");
     let Files = fs.readdirSync(Folderpath);
-    console.log(Files);
+    for (let i = 0; i < Files.length; i++) {
+        let ext = path.extname(Files[i]);
+        let nameofFolder = giveFolderName(ext);
+        console.log("Ext--",ext," Folder--",nameofFolder);
+    }
+
 } else {
     console.log("Please Enter a valid Path!!!!!!!");
+}
+
+
+
+function giveFolderName(ext){
+
+    for (let key in extensions)//For-each loop
+    {
+        let extArr = extensions[key];
+        for(let i = 0; i <extArr.length ; i++){
+            if (extArr[i] == ext){
+                return key;
+            }
+        }
+    }
+    return "Others";
+
 }
